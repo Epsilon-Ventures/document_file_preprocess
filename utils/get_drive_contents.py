39f74488e38de51bac1788f74d154aca1c46de6c
Nischal Shakya <nischalshakya2015@gmail.com>
2022-12-29 16:22:45 +0545

    Increase file return limit to 1000

--- utils/get_drive_contents.py
+++ utils/get_drive_contents.py
@@ -5,7 +5,9 @@ def get_all_files(drive_service, folder_id):
       drive_service: a Google Drive service object
       folder_id: the id of the folder
   """
-  drive_result = drive_service.files().list(q=f"'{folder_id}' in parents",fields='nextPageToken, files(name, id, mimeType)').execute()
+  # Page size is by default 100 and max is 1000
+  # Page size refers to max number of files returned in a single request
+  drive_result = drive_service.files().list(q=f"'{folder_id}' in parents",fields='nextPageToken, files(name, id, mimeType)', pageSize=1000).execute()
   items = drive_result.get('files', [])
 
   # Get all the files
